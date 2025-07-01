afcc9c169b260caf31a2d9ea4202c5a37822312c
acalabro <acalabro@gatto>
2025-07-01 11:09:12 +0200

    update ping

--- src/it/cnr/isti/labsedc/transponder/WiFiScanner.java
+++ src/it/cnr/isti/labsedc/transponder/WiFiScanner.java
@@ -99,9 +99,8 @@ public class WiFiScanner extends Thread {
 							if(LoRaAnd4GDispatcher.toSendBuffer.size() == 0) {
 								LoRaAnd4GDispatcher.addToBuffer("#BSSID:00:00:00:00:00:00&-00dBm&0000.00000,00000.00000#");
 								System.out.println("For checking connection, an empty message will be sent.");
-								lastMessageTime = System.currentTimeMillis();
 							}
-							
+							lastMessageTime = System.currentTimeMillis();							
 						}
 					}
 		    	}).start();
