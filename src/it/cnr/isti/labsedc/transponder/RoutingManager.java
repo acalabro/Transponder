4ff932f3d7cd7f092b72222877031936c1b1e1e2
acalabro <antonello.calabro@isti.cnr.it>
2025-09-25 08:58:44 +0200

    update routing

--- src/it/cnr/isti/labsedc/transponder/RoutingManager.java
+++ src/it/cnr/isti/labsedc/transponder/RoutingManager.java
@@ -2,11 +2,32 @@ package it.cnr.isti.labsedc.transponder;
 
 public class RoutingManager extends Thread {
 
-	public RoutingManager() {
-		
+	private CHANNELMODE transmissionMode;
+
+	public RoutingManager(CHANNELMODE transmissionMode) {
+		this.transmissionMode = transmissionMode;
 	}
 	
 	public void run() {
+		switch (transmissionMode) {
+		case LORA_ONLY: {
+					
+		}
+		case UMTS_LGE_4G_ONLY: {
+			
+		}
+		case SAT_ONLY: {
+			
+		}
+		case MULTI_CASCADE: {
+			
+		}
+		case MULTI_SIMULTANEOUS: {
+			
+		}
 		
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + transmissionMode);
+		}
 	}
 }
